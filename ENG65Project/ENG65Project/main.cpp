04a699a0e1a0e5c9fead432b05e399a5d4c6d0f7
josienordrum <jn.17@dartmouth.edu>
2016-02-22 22:03:55 -0500

    added main

---
+++ ENG65Project/ENG65Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ENG65Project
+//
+//  Created by Josephine Nordrum on 2/22/16.
+//  Copyright © 2016 Josephine Nordrum. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
