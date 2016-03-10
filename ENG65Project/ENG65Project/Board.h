f42013b9e51c0dc0b1f137112d9ab871b6b70e3b
josienordrum <jn.17@dartmouth.edu>
2016-03-09 21:11:01 -0500

    board now works-- all code compilesgit add Board.h

--- ENG65Project/ENG65Project/Board.h
+++ ENG65Project/ENG65Project/Board.h
@@ -37,48 +37,49 @@ public:
         //	ship = locations;
     }
     
-    void processcoordinates(string stringin) {
+    void processcoordinates(std::string stringin) {
         char x = stringin[0];
         int y = stringin[1];
         int index = (x-1)*dimension + y;
         
-        if (blocks[index].status != notchecked){
-            cout << "You've already checked that location! Try another spot:" << endl;
-            string input;
-            cin >> input;
-            processcoordinates(string input);
+        if (blocks[index].getstatus() != notchecked){
+            std::cout << "You've already checked that location! Try another spot" << std::endl;
+            std::string input;
+            std::cin >> input;
+            processcoordinates(input);
         }
-        switch (blocks[index].type) {
+        switch (blocks[index].gettype()) {
             case engine:
-                ships[blocks[index].shipnumber].sinkship();                //if its an engine, sink the whole ship
+                ships[blocks[index].getshipnumber()].sinkship();                //if its an engine, sink the whole ship
+                
                 break;
             case deck:
-                blocks[index].status = hitship;
+                blocks[index].setstatus(hitship);
                 break;
             case artillery:
-                blocks[index].status = hitship;
+                blocks[index].setstatus(hitship);
                 //lose turn code here
                 break;
             case water:
-                blocks[index].status = hitwater;
+                blocks[index].setstatus(hitwater);
         }
     }
     
     
     void printBoard(){
         // for the first row, print out column information
-        cout << "   ";                      //// print out the board to the screen
-        for(int j=0; j < dimension; j++) { cout << " " << j+1 << " "; }
-        cout << endl;
+        std::cout << "   ";                      //// print out the board to the screen
+        for(int j=0; j < dimension; j++) { std::cout << " " << j+1 << " "; }
+        std::cout << std::endl;
       
         for(int i=0; i < dimension; i++) {               // then, print row by row
-            cout << " " << (char)(i+65) << " ";			// print row character
+            std::cout << " " << (char)(i+65) << " ";			// print row character
             for(int j=0; j < dimension; j++) {
                 int index = (i-1)*dimension + j;
                 blocks[index].printblock();
             }
         }
-        cout << endl;		// change line at the end of the row
+        std::cout << std::endl;		// change line at the end of the row
     }
 };
 
