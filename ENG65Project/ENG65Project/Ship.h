a9c2e0e334d0a00f3bda30161ea8d126a3c934c7
Jenny Hyun Ji Seong <jennyseong77@gmail.com>
2016-03-12 00:17:31 -0500

    Formatting
    Added comments and corrected function names for readability

--- ENG65Project/ENG65Project/Ship.h
+++ ENG65Project/ENG65Project/Ship.h
@@ -3,29 +3,30 @@
 //  ENG65Project
 //
 //  Created by Josephine Nordrum on 3/7/16.
+//  Edited by Jenny Seong 3/11/16.
 //  Copyright © 2016 Josephine Nordrum. All rights reserved.
 //
 
 #ifndef Ship_h
 #define Ship_h
+
 #include "Block.h"
 #include <string>
+using namespace std;
 
 class ship {
 private:
-    std::string name;                //ships should have names
-    int size;                   //how many squares does the ship take up?
-    int *blockindicies;              //array of the ship's components
+    string name;                     //ships should have names
+    int size;                        //how many squares does the ship take up?
+    int *blockIndices;               //array of the ship's components
     
 public:
-    
-    ship(int givensize, std::string name);
-    ship();
-    ~ship();
-    int* getblocks();               //getters and setters for private members of block
-    int  getsizes();
-    void setblock( int index);
-    
+    ship(int givensize, std::string nInput);		// constructor with size and name
+    ship();											// default constructor
+    ~ship();										// destructor
+    int* getBlocks();								// returns block array
+    int  getSize();									// returns size of ship
+    void setBlock(int index[]);						// set blocks for ship
 };
 
 
