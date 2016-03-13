f7e32c5fa16d5770ec6919c2de195f3a8569f806
Jenny Hyun Ji Seong <jennyseong77@gmail.com>
2016-03-13 18:37:19 -0400

    Removed unnecessary includes
    already included in the h/hpp files that its importing

--- ENG65Project/ENG65Project/main.cpp
+++ ENG65Project/ENG65Project/main.cpp
@@ -12,14 +12,10 @@
 #include "PrintFns.hpp"
 #include "BoardSetUp.hpp"
 #include "TakeTurn.hpp"
-#include "stdio.h"
 #include <iostream>
 #include <array>
-using namespace std;
-
 
 enum player {user, opponent};
-
 
 int main(int argc, const char * argv[]) {
 	
