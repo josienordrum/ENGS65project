8c1d836442c88b305eca5461478188b800fa2e88
Jenny Hyun Ji Seong <jennyseong77@gmail.com>
2016-03-13 21:51:33 -0400

    Remove asking ship sizes
    Thought it was inefficient and confusing since it will be taking the
    answer as a string… It will be better to deal with one ship at once.

--- ENG65Project/ENG65Project/BoardSetUp.cpp
+++ ENG65Project/ENG65Project/BoardSetUp.cpp
@@ -13,16 +13,16 @@ using namespace std;
 
 void BoardSetUp(string player1, board board1, string shipsizes){
     int gamesetup = 0;                                  //game set up is not done so gamesetup = 0
-    cout <<endl << endl << "Hello "<< player1 << "! Let's set up your board! Let's start with your ship of size " << shipsizes[0] << endl;
+    cout <<endl << endl << "Hello "<< player1 << "! Let's set up your board!" << endl;
     board1.printBoard(gamesetup);
     int temp = shipsizes[0]-48;
-    board1.PlaceShips(temp,0);
+    board1.PlaceShips(0);
     board1.printBoard(gamesetup);
     
     for( int i = 1; i < board1.getDimension()/3; i++){
         cout << "The next ship you entered is of size " << shipsizes[i] << ". ";
         temp = shipsizes[i]-48;
-        board1.PlaceShips(temp, i);
+        board1.PlaceShips(i);
         board1.printBoard(gamesetup);
     }
 }
